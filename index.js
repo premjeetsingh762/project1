aab1ceb02f3ee96f193e465c96e5944b084112b5
premjeetsingh762 <49304470+premjeetsingh762@users.noreply.github.com>
2020-08-31 13:24:32 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Todos from './App';
+import { combineReducers } from "redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const songsReducer = () => {
+  return [
+    {
+      title: "No Scrubs",
+      duration: "4:05"
+    },
+    {
+      title: "Lily",
+      duration: "3:46"
+    },
+    {
+      title: "Faded",
+      duration: "3:33"
+    },
+    {
+      title: "I Want it That Way",
+      duration: "1:56"
+    }
+  ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === "SONG_SELECTED") {
+    return action.payload;
+  }
+
+  return selectedSong;
+};
+
+export default combineReducers({
+  songs: songsReducer,
+  selectedSong: selectedSongReducer
+})
